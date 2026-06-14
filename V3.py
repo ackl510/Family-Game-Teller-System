b4217ba3c40eeafc052bad96fae6f32d20f8a296
ackl510 <137654373+ackl510@users.noreply.github.com>
2026-06-14 12:08:35 -0400

    Refactor V3.py for better player management
    Refactor bank manager game to improve structure and readability.

--- V3.py
+++ V3.py
@@ -1,9 +1,6 @@
 import random
-import time
 
-start = False
 print("Welcome to the Bank Manager!")
-time.sleep(0.5)
 
 # --- Setup number of players ---
 num_players = int(input("How many players? "))
@@ -14,19 +11,11 @@ starting_money = int(input("Starting money for each player? "))
 # --- Create players dynamically ---
 players = {}
 for i in range(1, num_players + 1):
-    players[f"Player_{i}"] = starting_money
+    players["Player_" + str(i)] = starting_money
 
 print("Players created:", ", ".join(players.keys()))
-time.sleep(2)
-time.sleep(2)
 print("This system is used to manage the money of players in a game")
-time.sleep(2)
-print("This system is used to charge players, give them money, transfer money between players, and declare bankruptcy for players")
-time.sleep(2)
-print("The commands are based on a /{command} system")
-time.sleep(2)
 print("Type /help for a list of commands")
-
 
 print("Commands:")
 print("/roll")
@@ -35,45 +24,51 @@ print("/give")
 print("/transfer")
 print("/bankrupt")
 print("/end")
-start = True
+
 # --- Main loop ---
-while start:
+while True:
     command = input("Enter Command: ")
 
+    # Charge a player
     if command == "/charge":
         name = input("Who would you like to charge? ")
         amount = int(input("How much? "))
         players[name] -= amount
-        print(name, "now has $", players[name])
+        print(name + " now has $" + str(players[name]))
 
+    # Give money to a player
     elif command == "/give":
         name = input("Who would you like to give money to? ")
         amount = int(input("How much? "))
         players[name] += amount
-        print(name, "now has $", players[name])
+        print(name + " now has $" + str(players[name]))
 
+    # Transfer money between players
     elif command == "/transfer":
         sender = input("Transfer FROM: ")
         receiver = input("Transfer TO: ")
         amount = int(input("How much? "))
         players[sender] -= amount
         players[receiver] += amount
-        print(sender, "now has $", players[sender])
-        print(receiver, "now has $", players[receiver])
+        print(sender + " now has $" + str(players[sender]))
+        print(receiver + " now has $" + str(players[receiver]))
 
+    # Declare bankruptcy
     elif command == "/bankrupt":
         name = input("Who is bankrupt? ")
         players[name] = 0
-        print(name, "now has $0")
+        print(name + " now has $0")
 
+    # Roll dice
     elif command == "/roll":
-        d1 = random.randint(1,6)
-        d2 = random.randint(1,6)
-        print("You rolled", d1, "and", d2, "=", d1+d2)
+        d1 = random.randint(1, 6)
+        d2 = random.randint(1, 6)
+        print("You rolled", d1, "and", d2, "=", d1 + d2)
 
         if d1 == d2:
             print("Doubles! Roll again!")
 
+    # Help menu
     elif command == "/help":
         print("Commands:")
         print("/roll")
@@ -83,5 +78,13 @@ while start:
         print("/bankrupt")
         print("/end")
 
+    # End game
     elif command == "/end":
         print("Final totals:")
+        for name in players:
+            print(name + " has $" + str(players[name]))
+        break
+
+    # Unknown command
+    else:
+        print("Unknown command. Type /help")
